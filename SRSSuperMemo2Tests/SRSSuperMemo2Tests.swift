20dd48d8314c04aa0d1bb03db267b714b2826acc
Martin Nguyen <martin@maddin.io>
2017-06-02 21:49:22 +0200

    Added unit tests.

--- SRSSuperMemo2Tests/SRSSuperMemo2Tests.swift
+++ SRSSuperMemo2Tests/SRSSuperMemo2Tests.swift
@@ -10,27 +10,67 @@ import XCTest
 @testable import SRSSuperMemo2
 
 class SRSSuperMemo2Tests: XCTestCase {
+
+    var sm2Engine = SM2Engine()
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testEasinessFactor() {
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.67), grade: 3).easinessFactor < 1.67) 
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.67), grade: 4).easinessFactor == 1.67) 
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.67), grade: 5).easinessFactor > 1.67) 
+
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.89), grade: 3).easinessFactor < 1.89)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.89), grade: 4).easinessFactor == 1.89)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.89), grade: 5).easinessFactor > 1.89)
+
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.2), grade: 3).easinessFactor < 2.2)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.2), grade: 4).easinessFactor == 2.2)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.2), grade: 5).easinessFactor > 2.2)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testMinimumEasinessFactor() {
+        let minimumEasinessFactor = 1.3
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.3), grade: 0).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.3), grade: 1).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.3), grade: 2).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.3), grade: 3).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.3), grade: 4).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.3), grade: 5).easinessFactor > minimumEasinessFactor)
+
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1.0), grade: 2).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 0.0), grade: 0).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 0.0), grade: 1).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 0.0), grade: 2).easinessFactor == minimumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 0.0), grade: 3).easinessFactor == minimumEasinessFactor)
+    }
+
+    func testMaximumEasinessFactor() {
+        let maximumEasinessFactor = 2.5
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.5), grade: 0).easinessFactor == maximumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.5), grade: 1).easinessFactor == maximumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.5), grade: 2).easinessFactor == maximumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.5), grade: 3).easinessFactor < maximumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.5), grade: 4).easinessFactor == maximumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.5), grade: 5).easinessFactor == maximumEasinessFactor)
+
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 3.0), grade: 5).easinessFactor == maximumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 1000.0), grade: 5).easinessFactor == maximumEasinessFactor)
+        XCTAssert(gradedItem(item: SRSItem(easinessFactor: 2.6), grade: 5).easinessFactor == maximumEasinessFactor)
+    }
+
+    func gradedItem(item: SRSItem, grade: SM2Engine.Grade) -> SRSItem {
+        return sm2Engine.gradeSRSItem(item: item, grade: grade)
     }
     
 }
