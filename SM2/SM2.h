a00d3232bcc174e919aa882bbaa8613a2018f031
Martin Nguyen <martin@maddin.io>
2017-06-04 16:36:20 +0200

    Added framework target so project can be added as a sub-project.

---
+++ SM2/SM2.h
@@ -0,0 +1,19 @@
+//
+//  SM2.h
+//  SM2
+//
+//  Created by Maddin on 04/06/2017.
+//  Copyright © 2017 Maddin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SM2.
+FOUNDATION_EXPORT double SM2VersionNumber;
+
+//! Project version string for SM2.
+FOUNDATION_EXPORT const unsigned char SM2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SM2/PublicHeader.h>
+
+
